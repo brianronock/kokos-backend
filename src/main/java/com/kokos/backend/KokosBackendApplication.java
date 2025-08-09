74e74abe50d81a88e9af7029b347febb350a80a4
Brian Rono C. K. <bkcrono@gmail.com>
2025-08-09 20:05:13 +0200

    Initialize Spring Boot backend with Maven

---
+++ src/main/java/com/kokos/backend/KokosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kokos.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KokosBackendApplication.class, args);
+    }
+
+}
